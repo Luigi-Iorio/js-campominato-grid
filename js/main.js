84e6fdf1e015dcb60cdcd7459c8a238a6b26d761
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-15 16:45:43 +0100

    feat: creazione griglia al click del bottone play

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 "use strict";
+
+/*
+FUNZIONI
+*/
+
+// per creare la cella all'interno del container
+function creazioneCella(tag, nomeClasse, contenuto) {
+  const cella = document.createElement(tag);
+  cella.classList.add(nomeClasse);
+  cella.append(contenuto);
+
+  return cella;
+}
+
+/*
+PROGRAMMA
+*/
+
+// variabile per il bottone
+const bottonePlay = document.querySelector(".play");
+
+// evento al click del bottone
+bottonePlay.addEventListener("click", function () {
+  // variabile per il titolo
+  const titolo = document.querySelector("h2");
+  titolo.classList.add("display-none");
+
+  // variabile per il container
+  const container = document.querySelector(".container");
+  container.classList.remove("opacity-0");
+  container.classList.add("opacity-1");
+
+  // per creare celle dinamicamente e assegnazione numero alle celle
+  for (let i = 1; i <= 100; i++) {
+    const cella = creazioneCella("div", "cella", i);
+    container.append(cella);
+  }
+});
